e61ff9b7ac072a7179c2257542a63cc3aaf670b2
harshit2024 <harshitgsrivastava07@gmail.com>
2024-04-14 00:40:42 +0530

    Horizontal Scroller View added

--- secondApp/components/Scroller.tsx
+++ secondApp/components/Scroller.tsx
@@ -1,12 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import FlatCards from './FlatCards'
 
 export default function Scroller() {
     return (
         <View>
-            <Text>Scroller</Text>
+            <Text style={styles.header}>Scroller</Text>
+            <ScrollView horizontal={true}>
+                <View style={[styles.card, styles.elevatedCard]}>
+                    <Text>Card 1</Text>
+                </View>
+                <View style={[styles.card, styles.elevatedCard]}>
+                    <Text>Card 2</Text>
+                </View>
+                <View style={[styles.card, styles.elevatedCard]}>
+                    <Text>Card 3</Text>
+                </View>
+                <View style={[styles.card, styles.elevatedCard]}>
+                    <Text>Card 4</Text>
+                </View>
+                <View style={[styles.card, styles.elevatedCard]}>
+                    <Text>Card 5</Text>
+                </View>
+
+            </ScrollView>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 15,
+    },
+    card: {
+        width: 100,
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 15,
+
+    },
+    elevatedCard: {
+        backgroundColor: '#FFC3A0',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 1,
+            height: 1,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    }
+})
